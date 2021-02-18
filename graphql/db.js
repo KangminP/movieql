f17a663e52ca437d7e0584091a8a923762b8becf
Kangmin <mygangmini@naver.com>
2021-02-18 16:43:53 +0900

    add addMovie, deleteMovie function

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,49 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Kangmin",
-        age: 29,
-        gender: "male"
+        id: 0,
+        name: "Star Wars - The new one",
+        score: 7
     },
     {
-        id: "1",
-        name: "Junhyuck",
-        age: 29,
-        gender: "male"
+        id: 1,
+        name: "Avengers - The new one",
+        score: 8
     },
     {
-        id: "2",
-        name: "Kyunam",
-        age: 29,
-        gender: "male"
+        id: 2,
+        name: "The Godfather 1",
+        score: 5
     },
     {
-        id: "3",
-        name: "Seokchan",
-        age: 26,
-        gender: "male"
+        id: 3,
+        name: "Logan",
+        score: 4
     },
-    {
-        id: "4",
-        name: "Yako",
-        age: 27,
-        gender: "female"
-    }
 ]
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id))
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(movie => movie.id === id)
+    return filteredMovies[0]
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id)
+    if(movies.length > cleanedMovies.length){
+        movies = cleanedMovies
+        return true
+    } else {
+        return false
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    }
+    movies.push(newMovie)
+    return newMovie
 }
